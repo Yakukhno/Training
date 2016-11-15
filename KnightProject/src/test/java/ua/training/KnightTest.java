d55ee42ad6bf61c22d4fb0a5993fa08838938083
Ivan Yakukhno <sokole2@yandex.ru>
2016-11-15 19:05:31 +0200

    added tests to Knight methods  KnightProject

---
+++ KnightProject/src/test/java/ua/training/KnightTest.java
@@ -0,0 +1,65 @@
+package ua.training;
+
+import org.junit.Before;
+import org.junit.Test;
+import ua.training.models.ammunition.*;
+import ua.training.models.knight.IKnight;
+import ua.training.models.knight.Knight;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class KnightTest {
+
+    IKnight knight;
+
+    @Before
+    public void before() {
+        knight = new Knight();
+    }
+
+    @Test
+    public void testAmmunitionCost() {
+
+        ChainArmor chainArmor = new ChainArmor(200, 23, "steal");
+        Helmet helmet = new Helmet(80, 4, false);
+        Shield shield = new Shield(100, 3, "wood", Shield.Shape.SQUARE);
+        Weapon weapon = new Weapon(150, 10);
+
+        knight.setChainArmor(chainArmor);
+        knight.setWeapon(weapon);
+
+        assertEquals(knight.ammunitionCost(), chainArmor.getPrice()
+                + weapon.getPrice());
+
+        knight.setHelmet(helmet);
+        knight.setShield(shield);
+
+        assertEquals(knight.ammunitionCost(), chainArmor.getPrice()
+                + helmet.getPrice() + shield.getPrice() + weapon.getPrice());
+
+    }
+
+    @Test
+    public void testSortByWeight() {
+
+        ChainArmor chainArmor = new ChainArmor(200, 23, "steal");
+        Helmet helmet = new Helmet(80, 4, false);
+        Shield shield = new Shield(100, 3, "wood", Shield.Shape.SQUARE);
+        Weapon weapon = new Weapon(150, 10);
+
+        IAmmunition[] ammunitionArray = new IAmmunition[] {
+                shield, helmet, weapon, chainArmor
+        };
+
+        knight.setChainArmor(chainArmor);
+        knight.setWeapon(weapon);
+        knight.setHelmet(helmet);
+        knight.setShield(shield);
+
+        Set<IAmmunition> setAmmunition = knight.sortByWeight();
+
+        assertArrayEquals(setAmmunition.toArray(), ammunitionArray);
+    }
+
+}
